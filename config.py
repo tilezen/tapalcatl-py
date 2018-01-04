7cef1e6a875ba5b2b26e5e6f606d5bf79c69e080
Ian Dees <ian.dees@gmail.com>
2018-01-03 22:35:05 -0600

    Pass through SERVER_NAME to Flask config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 CORS_SEND_WILDCARD = True
 CACHE_MAX_AGE = int(os.environ.get("CACHE_MAX_AGE", '600'))
+SERVER_NAME = os.environg.get('SERVER_NAME')
 S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_PREFIX = os.environ.get("S3_PREFIX")
 METATILE_SIZE = int(os.environ.get("METATILE_SIZE", '4'))
