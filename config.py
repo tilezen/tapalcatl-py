9b7def64203f91a4561df3f32f93ad3d319fd1d3
Ian Dees <ian.dees@gmail.com>
2018-01-03 23:34:34 -0600

    Pass through TILES_URL_BASE envvar

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 CORS_SEND_WILDCARD = True
 CACHE_MAX_AGE = int(os.environ.get("CACHE_MAX_AGE", '600'))
+TILES_URL_BASE = os.environ.get('TILES_URL_BASE')
 S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_PREFIX = os.environ.get("S3_PREFIX")
 METATILE_SIZE = int(os.environ.get("METATILE_SIZE", '4'))
