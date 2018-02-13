07235c015d2e140f6cf9a090eb38a3f3c10020da
Nathaniel V. KELSO <nvkelso@gmail.com>
2018-02-12 18:49:35 -0800

    Set default max age to 4 weeks, shared max to 2 weeks

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ CORS_SEND_WILDCARD = True
 # The max age for tiles returned by this service. Clients (both browsers and intermediate caches like CloudFront) will
 # cache the tile this many seconds before checking with the origin to get a new tile.
 # http://werkzeug.pocoo.org/docs/0.14/datastructures/#werkzeug.datastructures.ResponseCacheControl.max_age
-CACHE_MAX_AGE = int(os.environ.get("CACHE_MAX_AGE", '1200'))
+CACHE_MAX_AGE = int(os.environ.get("CACHE_MAX_AGE", '2419200'))
 
 # The "shared" max age for tiles returned by this service. When an object is beyond this age in a shared cache (like CloudFront),
 # the shared cache should check with the origin to see if the object was updated. In general, this number should be smaller than
 # the max age set above.
 # http://werkzeug.pocoo.org/docs/0.14/datastructures/#werkzeug.datastructures.ResponseCacheControl.s_maxage
-SHARED_CACHE_MAX_AGE = int(os.environ.get("SHARED_CACHE_MAX_AGE", '600'))
+SHARED_CACHE_MAX_AGE = int(os.environ.get("SHARED_CACHE_MAX_AGE", '1209600'))
 
 METATILE_CACHE_SIZE = int(os.environ.get('METATILE_CACHE_SIZE', 0)) * 1000 * 1000
 TILES_URL_BASE = os.environ.get('TILES_URL_BASE')
